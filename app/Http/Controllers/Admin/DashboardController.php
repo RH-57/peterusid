30c2e91cc7fbbc79620a8649db85a483740f3a14
rahmat <rahmatbachtiar64@gmail.com>
2025-05-22 13:04:36 +0700

    Show visitor order by Ip Address

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
 {
     public function index() {
 
-        $visits = Visit::count();
+        $visits = Visit::distinct('ip_address')->count();
 
         return view('admin.index', compact('visits'));
     }
